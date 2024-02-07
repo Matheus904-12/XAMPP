70ca71dc729097eab4d093a5ba55b3ef0743ab80
Matheus Lucindo dos Santos <67837282+Matheus904-12@users.noreply.github.com>
2024-02-07 15:17:02 -0300

    ..

---
+++ autenticacao.php
@@ -0,0 +1,15 @@
+<?php
+$login=$_POST["login"];
+$senha=$_POST["senha"];
+
+
+
+    if ($login=='matheus' && $senha==145836){
+
+    echo "Seja bem vindo ".$login;
+
+    } else {
+        echo "Senha e/ou Usuário Incorretos."
+    }
+
+?>
